3f184e3a1ae4bca3935cd55b28581c98cb85bc2e
cmes31419 <cmes31419@gmail.com>
2026-04-17 01:11:55 +0800

    fix

--- CVSD_final_team011/bch_q6_only/01_RTL/rotate.sv
+++ CVSD_final_team011/bch_q6_only/01_RTL/rotate.sv
@@ -82,35 +82,3 @@ module rotate_16(
     end
 
 endmodule
-
-module rotate_24(
-    input  [5:0]        sigma,
-    output reg [5:0]    sigma_rot
-);
-
-    always @(*) begin
-        sigma_rot[0] = sigma[0] ^ sigma[2];
-        sigma_rot[1] = sigma[1] ^ sigma[2] ^ sigma[3];
-        sigma_rot[2] = sigma[2] ^ sigma[3] ^ sigma[4];
-        sigma_rot[3] = sigma[3] ^ sigma[4] ^ sigma[5];
-        sigma_rot[4] = sigma[0] ^ sigma[4] ^ sigma[5];
-        sigma_rot[5] = sigma[1] ^ sigma[5];
-    end
-
-endmodule
-
-module rotate_32(
-    input  [5:0]        sigma,
-    output reg [5:0]    sigma_rot
-);
-
-    always @(*) begin
-        sigma_rot[0] = sigma[0] ^ sigma[3];
-        sigma_rot[1] = sigma[1] ^ sigma[3] ^ sigma[4];
-        sigma_rot[2] = sigma[2] ^ sigma[4] ^ sigma[5];
-        sigma_rot[3] = sigma[0] ^ sigma[3] ^ sigma[5];
-        sigma_rot[4] = sigma[1] ^ sigma[4];
-        sigma_rot[5] = sigma[2] ^ sigma[5];
-    end
-
-endmodule
