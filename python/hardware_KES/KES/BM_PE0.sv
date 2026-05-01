6aed15c8c93399662086ab9d5b1d990527fdf651
jackieleong <jackieleong030114@gmail.com>
2026-05-02 01:47:02 +0800

    added riBM verilog (not tested)

---
+++ python/hardware_KES/KES/BM_PE0.sv
@@ -0,0 +1,52 @@
+module BM_PE0(
+    input clk,
+    input rst,
+    input syndrome_rdy,
+    
+    input [5:0] gamma,
+    input [5:0] discrepancy,
+    input branch,
+
+    input [5:0] sigma_init,
+    input [5:0] b_poly_in,
+
+    output [5:0] b_poly_out,
+    output [5:0] sigma_poly_out
+);
+
+
+reg [5:0] b_poly, b_poly_next, sigma_poly, sigma_poly_next;
+wire [5:0] gfmul0_out, gfmul1_out;
+
+
+assign sigma_poly_out = sigma_poly;
+assign b_poly_out = b_poly;
+
+gf_mul u_gfmul0(.in1(gamma), in2(sigma_poly), .prod(gfmul0_out));
+gf_mul u_gfmul1(.in1(b_poly_in), in2(discrepancy), .prod(gfmul1_out));
+
+
+always @(*) begin
+    if (syndrome_rdy) begin
+        sigma_poly_next = sigma_init;
+        b_poly_next = 0;
+    end
+    else begin
+        sigma_poly_next = gfmul0_out ^ gfmul1_out;
+        b_poly_next = (branch)? sigma_poly : b_poly_in;
+    end
+end
+
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        b_poly      <= 0;
+        sigma_poly  <= 0;
+    end
+
+    else begin
+        b_poly      <= b_poly_next;
+        sigma_poly  <= sigma_poly_next;
+    end
+end
+
+endmodule
