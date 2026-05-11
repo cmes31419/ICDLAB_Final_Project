369f0410416cb1d77c048ee5b5296e779f90c998
cmes31419 <cmes31419@gmail.com>
2026-05-11 10:52:32 +0800

    fix CHIP.sv in 01_RTL

--- 01_RTL/CHIP.sv
+++ 01_RTL/CHIP.sv
@@ -8,17 +8,18 @@ module CHIP(
     output          ovalid
 );
 
-    reg [4:0]   iaddr, iaddr_next;
+    reg [4:0]   icnt, icnt_next;
 
+    wire [4:0]  iaddr;
     wire [62:0] cdata;
     wire        cdone, sdone;
-    wire [5:0] LO_syn[3:0];
-    wire LKES_done;
-    wire [5:0] cs_sigma_in [6:0], LKES_sigma_out[2:0];
+    wire [5:0]  LO_syn[3:0];
+    wire        LKES_done;
+    wire [5:0]  cs_sigma_in [6:0], LKES_sigma_out[2:0];
     
-
     // temporaily set to only LKES
     genvar gi;
+
     generate
         for (gi=0; gi<3;gi=gi+1) begin
             assign cs_sigma_in[gi] = LKES_sigma_out[gi];
@@ -27,6 +28,9 @@ module CHIP(
             assign cs_sigma_in[gi] = 0;
         end
     endgenerate
+
+    assign iaddr = {icnt[4:3], 3'h7 - icnt[2:0]};
+
     assign iready = 1;
 
     memory mem0(
@@ -45,7 +49,7 @@ module CHIP(
     syndrome syn0(
         .clk(clk),
         .rst(rst),
-        .cnt(iaddr[2:0]),
+        .cnt(icnt[2:0]),
         .idata(idata),
         .ivalid(ivalid),
         .S(LO_syn),
@@ -73,16 +77,16 @@ module CHIP(
     );
 
     always @(*) begin
-        if (ivalid) iaddr_next = iaddr + 1;
-        else iaddr_next = iaddr;
+        if (ivalid) icnt_next = icnt + 1;
+        else icnt_next = icnt;
     end
 
     always @(posedge clk or posedge rst) begin
         if (rst) begin
-            iaddr <= 0;
+            icnt    <= 0;
         end
         else begin
-            iaddr <= iaddr_next;
+            icnt    <= icnt_next;
         end
     end
     
