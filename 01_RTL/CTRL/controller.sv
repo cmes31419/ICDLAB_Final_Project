061951ce720a2c327d9cea35674faee3275f061f
cmes31419 <cmes31419@gmail.com>
2026-06-05 17:41:06 +0800

    fix conflicts

--- 01_RTL/CTRL/controller.sv
+++ 01_RTL/CTRL/controller.sv
@@ -5,7 +5,10 @@ module controller(
     input           ovalid,
     input           sdone,
     input           LKES_done,
+<<<<<<< HEAD
     input           LKES_fail,
+=======
+>>>>>>> fdb6577 (modify many modules)
     input           cdone,
     input           cfail,
     input [3:0]     nflag,
